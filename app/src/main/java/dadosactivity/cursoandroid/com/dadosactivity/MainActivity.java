0b5b30c525ad57cb95face6e8c6bf705df6afb5a
henriquemalone <henrique.malone@gmail.com>
2018-08-03 13:14:34 -0300

    Initial commit

---
+++ app/src/main/java/dadosactivity/cursoandroid/com/dadosactivity/MainActivity.java
@@ -0,0 +1,70 @@
+package dadosactivity.cursoandroid.com.dadosactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao = (Button) findViewById(R.id.botaoId);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+                intent.putExtra("nome", "Henrique");
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void recreate() {
+        super.recreate();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+}
---
+++ app/src/main/java/dadosactivity/cursoandroid/com/dadosactivity/SegundaActivity.java
@@ -0,0 +1,68 @@
+package dadosactivity.cursoandroid.com.dadosactivity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SegundaActivity extends Activity {
+
+    private TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        texto = (TextView) findViewById(R.id.textoId);
+
+        Bundle extra = getIntent().getExtras();
+
+        if(extra != null){
+            String textoPassado = extra.getString("nome");
+            texto.setText(textoPassado);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void recreate() {
+        super.recreate();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+}
